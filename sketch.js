c8ef9ca24705cfbef12e12f99e64fb2d57236576
WhoIsNirajan <100168957+WhoIsNirajan@users.noreply.github.com>
2022-03-21 17:13:28 -0400

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -12,9 +12,9 @@ function preload(){
   blue_balloonImage = loadImage("blue_balloon0.png");
 
   red_balloonImage = loadImage("red_balloon0.png");
-  red_balloonImage = loadImage("redballoon0.png");
-  red_balloonImage = loadImage("red_balloon0");
-  red_balloonImage = loadImage("red_balloon0.png");
+  //red_balloonImage = loadImage("redballoon0.png");
+  //red_balloonImage = loadImage("red_balloon0");
+  //red_balloonImage = loadImage("red_balloon0.png");
   
 }
 
@@ -51,15 +51,14 @@ function draw() {
    // release arrow when space key is pressed
   if (keyDown("space")) {
     createArrow();
-    arrow.velocityX=-3
   }
    
   //creating continous enemies
 
   var select_balloon = Math.round(random(1,4));
-  var select_balloon = random(1,4);
-  var select_balloon = Math.round(random());
-  var select_balloon = Math.round(random(1,4,2));
+  //var select_balloon = random(1,4);
+  //var select_balloon = Math.round(random());
+  //var select_balloon = Math.round(random(1,4,2));
   
   if (World.frameCount % 100 == 0) {
     if (select_balloon == 1) {
@@ -89,21 +88,21 @@ function draw() {
   arrow.scale = 0.3;
 }
 
- function redBalloon() {
-var red = createSprite(0,50, 10, 10);
-red.addImage(red_balloonImage);
-red.velocityX = 3;
-red.lifetime = 150;
-red.scale = 0.1;
-}
+// function redBalloon() {
+//var red = createSprite(0,50, 10, 10);
+//red.addImage(red_balloonImage);
+//red.velocityX = 3;
+//red.lifetime = 150;
+//red.scale = 0.1;
+//}
 
-function redBalloon() {
-var red = createSprite(Math.round(random(20, 370)),50, 10, 10);
-red.addImage(red_balloonImage);
-red.velocityX = 3;
-red.lifetime = 150;
-red.scale = 0.1;
-}
+//function redBalloon() {
+//var red = createSprite(Math.round(random(20, 370)),50, 10, 10);
+//red.addImage(red_balloonImage);
+//red.velocityX = 3;
+//red.lifetime = 150;
+//red.scale = 0.1;
+//}
 
 function redBalloon() {
 var red = createSprite(0,Math.round(random(20, 370)), 10, 10);
@@ -113,13 +112,13 @@ red.lifetime = 150;
 red.scale = 0.1;
 }
 
-function redBalloon() {
-var red = createSprite(0, 10, Math.round(random(20, 370)) , 10);
-red.addImage(red_balloonImage);
-red.velocityX = 3;
-red.lifetime = 150;
-red.scale = 0.1;
-}
+//function redBalloon() {
+//var red = createSprite(0, 10, Math.round(random(20, 370)) , 10);
+//red.addImage(red_balloonImage);
+//red.velocityX = 3;
+//red.lifetime = 150;
+//red.scale = 0.1;
+//}
 
 function blueBalloon() {
   var blue = createSprite(0,Math.round(random(20, 370)), 10, 10);
